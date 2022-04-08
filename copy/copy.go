826a5d38d9c6114e6d57e313eab1ef9b69b0f142
Tzuyang Tsai <shiyou0130011+github@googlemail.com>
2022-04-08 20:33:57 +0800

    修正 copy package 的 bug 以及讓 copy functions 儘量 return error

--- copy/copy.go
+++ copy/copy.go
@@ -2,6 +2,7 @@ package copy
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"io/ioutil"
 	"log"
@@ -10,18 +11,20 @@ import (
 	"path/filepath"
 )
 
-// Copy the file from sourceFolderPath to outputFolderPath
-func File(sourceFolderPath string, outputFolderPath string, relativeFilePath string) {
+// File is for copying the file from sourceFolderPath to outputFolderPath
+//
+// It will return error when the file cannot be copied
+func File(sourceFolderPath string, outputFolderPath string, relativeFilePath string) error {
 	log.Printf(`Copy file "%s" from "%s" to "%s"`, relativeFilePath, sourceFolderPath, outputFolderPath)
 	data, err := ioutil.ReadFile(path.Join(sourceFolderPath, relativeFilePath))
 	if err != nil {
-		return
+		return err
 	}
 
-	ioutil.WriteFile(path.Join(outputFolderPath, relativeFilePath), data, 0644)
+	return ioutil.WriteFile(path.Join(outputFolderPath, relativeFilePath), data, 0644)
 }
 
-// Copy the file (path is the FS's path) to outputFolderPath
+// FS is for copying the file (path is the FS's path) to outputFolderPath
 // It can be copy folder or file
 func FS(f embed.FS, path string, outputFolderPath string) {
 	fs.WalkDir(f, path, func(filePath string, d fs.DirEntry, err error) error {
@@ -52,11 +55,13 @@ func FS(f embed.FS, path string, outputFolderPath string) {
 	})
 }
 
-//Copy the source folder to output folder
-func Dir(sourceFolder string, outputFolder string) {
+// Dir is for Copying the source folder to output folder
+//
+// It will return error when one of sub-file or sub-folder cannot be copied
+func Dir(sourceFolder string, outputFolder string) error {
 	fileInfos, err := ioutil.ReadDir(sourceFolder)
 	if err != nil {
-		log.Printf("Cannot read folder %s", sourceFolder)
+		return fmt.Errorf("Cannot read folder %s", sourceFolder)
 	}
 	for _, fi := range fileInfos {
 		sourcePath := filepath.Join(sourceFolder, fi.Name())
@@ -64,13 +69,19 @@ func Dir(sourceFolder string, outputFolder string) {
 		if fi.IsDir() {
 			err = os.Mkdir(outPath, 0755)
 			if err != nil {
-				log.Printf("Cannot create folder %s", outPath)
-			} else {
-				Dir(sourcePath, outPath)
+				return fmt.Errorf("Cannot create folder %s", outPath)
+			}
+			err = Dir(sourcePath, outPath)
+			if err != nil {
+				return err
 			}
 
 		} else {
-			File(sourcePath, outputFolder, fi.Name())
+			err = File(sourceFolder, outputFolder, fi.Name())
+			if err != nil {
+				return err
+			}
 		}
 	}
+	return nil
 }
